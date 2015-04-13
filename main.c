b6740cc504756fd5beaea60ae4e32adfa0f14aed
I Putu Pradnyana <pradnyana12@mhs.if.its.ac.id>
2015-04-13 18:55:15 +0700

    Fixing indentation

--- main.c
+++ main.c
@@ -14,12 +14,12 @@
 #define PRINT_KEYS
 
 typedef struct Client{
-    char Name[256];
-    int sockfd;
+	char Name[256];
+	int sockfd;
 	char* public_key;
 	RSA* keypair;
-    struct Client* Next;
-    struct Client* Previous;
+	struct Client* Next;
+	struct Client* Previous;
 }Client;
 
 Client* head;
@@ -31,94 +31,94 @@ size_t public_len;
 void InitRSA();
 
 void* SomeAwesomeThings(void* Param){
-    Client* theClient = (Client*)Param;
-    char message[4086];
-    char sendMessage[4086];
+	Client* theClient = (Client*)Param;
+	char message[4086];
+	char sendMessage[4086];
 	char encrypt[RSA_size(keypair)];
-    char* receiver;
-    char* tmp;
+	char* receiver;
+	char* tmp;
 	char* err;
-    Client* ClientCounter;
-    int msgSize;
-    int usernameSet = 0;
-    int nameLength;
+	Client* ClientCounter;
+	int msgSize;
+	int usernameSet = 0;
+	int nameLength;
 	int decrypt_len;
 	int encrypt_len;
 
-    memset(sendMessage, '\0', sizeof(sendMessage));
+	memset(sendMessage, '\0', sizeof(sendMessage));
 	err = (char*) malloc(130);
 
-//    if((msgSize = read(theClient->sockfd, message, sizeof(message) - 1)) > 0){
-//        message[msgSize] = '\0';
-//        int nameLength = strstr(message, "\r\n.\r\n") - message;
-//        for(msgSize = 0; msgSize < nameLength; msgSize++){
-//            theClient->Name[msgSize] = message[msgSize];
-//        }
-//        theClient->Name[nameLength] = '\0';
-//        printf("%s has connected\n", theClient->Name);
-//    }
+	//    if((msgSize = read(theClient->sockfd, message, sizeof(message) - 1)) > 0){
+	//        message[msgSize] = '\0';
+	//        int nameLength = strstr(message, "\r\n.\r\n") - message;
+	//        for(msgSize = 0; msgSize < nameLength; msgSize++){
+	//            theClient->Name[msgSize] = message[msgSize];
+	//        }
+	//        theClient->Name[nameLength] = '\0';
+	//        printf("%s has connected\n", theClient->Name);
+	//    }
 
 
-    while((msgSize = read(theClient->sockfd, message, sizeof(message) - 1)) > 0){
+	while((msgSize = read(theClient->sockfd, message, sizeof(message) - 1)) > 0){
 
-        message[msgSize] = '\0';
-        printf("%s", message);
+		message[msgSize] = '\0';
+		printf("%s", message);
 
-        //Do things here
-        tmp = strstr(message, "\r\n");
-        *tmp = '\0';
-        if(strcmp(message, "Mode: Public") == 0){
+		//Do things here
+		tmp = strstr(message, "\r\n");
+		*tmp = '\0';
+		if(strcmp(message, "Mode: Public") == 0){
 
-            tmp = tmp + 2;
-            sprintf(sendMessage,"%s\r\nUser: %s\r\n%s", message, theClient->Name, tmp);
+			tmp = tmp + 2;
+			sprintf(sendMessage,"%s\r\nUser: %s\r\n%s", message, theClient->Name, tmp);
 
-            for(ClientCounter = head; ClientCounter != NULL; ClientCounter = ClientCounter->Next){
-                if(ClientCounter == theClient) continue;
-                write(ClientCounter->sockfd, sendMessage, sizeof(sendMessage));
-            }
-        }
-        else if((strcmp(message, "Mode: Private") == 0) || (strcmp(message, "Mode: InitPriv") == 0) || (strcmp(message, "Mode: AccPriv") == 0) ){
+			for(ClientCounter = head; ClientCounter != NULL; ClientCounter = ClientCounter->Next){
+				if(ClientCounter == theClient) continue;
+				write(ClientCounter->sockfd, sendMessage, sizeof(sendMessage));
+			}
+		}
+		else if((strcmp(message, "Mode: Private") == 0) || (strcmp(message, "Mode: InitPriv") == 0) || (strcmp(message, "Mode: AccPriv") == 0) ){
 
-            tmp = tmp + 2;
-            tmp = strstr(tmp, " ") + 1;
-            receiver = tmp;
-            tmp = strstr(tmp, "\r\n");
-            *tmp = '\0';
-            tmp = tmp + 2;
+			tmp = tmp + 2;
+			tmp = strstr(tmp, " ") + 1;
+			receiver = tmp;
+			tmp = strstr(tmp, "\r\n");
+			*tmp = '\0';
+			tmp = tmp + 2;
 
-            sprintf(sendMessage,"%s\r\nUser: %s\r\n%s", message, theClient->Name, tmp);
+			sprintf(sendMessage,"%s\r\nUser: %s\r\n%s", message, theClient->Name, tmp);
 
-            for(ClientCounter = head; ClientCounter != NULL; ClientCounter = ClientCounter->Next){
-                if(strcmp(receiver, ClientCounter->Name) == 0){
-                    write(ClientCounter->sockfd, sendMessage, sizeof(sendMessage));
-                    break;
-                }
-            }
-        }
-        else if(strcmp(message, "Mode: GetList") == 0){
-            sprintf(sendMessage, "Mode: List\r\n");
-            for(ClientCounter = head; ClientCounter != NULL; ClientCounter = ClientCounter->Next){
-                if(ClientCounter == theClient)continue;
-                strcat(sendMessage, ClientCounter->Name);
-                strcat(sendMessage, "\r\n");
-            }
-            strcat(sendMessage, "\r\n.\r\n");
+			for(ClientCounter = head; ClientCounter != NULL; ClientCounter = ClientCounter->Next){
+				if(strcmp(receiver, ClientCounter->Name) == 0){
+					write(ClientCounter->sockfd, sendMessage, sizeof(sendMessage));
+					break;
+				}
+			}
+		}
+		else if(strcmp(message, "Mode: GetList") == 0){
+			sprintf(sendMessage, "Mode: List\r\n");
+			for(ClientCounter = head; ClientCounter != NULL; ClientCounter = ClientCounter->Next){
+				if(ClientCounter == theClient)continue;
+				strcat(sendMessage, ClientCounter->Name);
+				strcat(sendMessage, "\r\n");
+			}
+			strcat(sendMessage, "\r\n.\r\n");
 
-            write(theClient->sockfd, sendMessage, sizeof(sendMessage));
-        }
-        else if(strcmp(message, "Mode: Username") == 0){
-            tmp = tmp + 2;
-            if(usernameSet == 0){
-                printf("Setting username\n");
-                nameLength = strstr(tmp, "\r\n.\r\n") - tmp;
-                for(msgSize = 0; msgSize < nameLength; msgSize++){
-                    theClient->Name[msgSize] = *(tmp + msgSize);
-                }
-                theClient->Name[nameLength] = '\0';
-                printf("%s\n", theClient->Name);
-                usernameSet++;
-            }
-        }
+			write(theClient->sockfd, sendMessage, sizeof(sendMessage));
+		}
+		else if(strcmp(message, "Mode: Username") == 0){
+			tmp = tmp + 2;
+			if(usernameSet == 0){
+				printf("Setting username\n");
+				nameLength = strstr(tmp, "\r\n.\r\n") - tmp;
+				for(msgSize = 0; msgSize < nameLength; msgSize++){
+					theClient->Name[msgSize] = *(tmp + msgSize);
+				}
+				theClient->Name[nameLength] = '\0';
+				printf("%s\n", theClient->Name);
+				usernameSet++;
+			}
+		}
 		else if(strcmp(message, "Mode: GetCA") == 0){
 			sprintf(sendMessage, "Mode: ServCA\r\n");
 			strcat(sendMessage, public_key);
@@ -165,88 +165,88 @@ void* SomeAwesomeThings(void* Param){
 				}
 			}
 		}
-    }
+	}
 
-    printf("%s has been disconnected\n", theClient->Name);
-    if(theClient == head){
-        head = theClient->Next;
-        if(head != NULL)head->Previous = NULL;
-    }
-    else{
-        theClient->Previous->Next = theClient->Next;
-        if(theClient == tail){
-            tail = theClient->Previous;
-        }
-    }
+	printf("%s has been disconnected\n", theClient->Name);
+	if(theClient == head){
+		head = theClient->Next;
+		if(head != NULL)head->Previous = NULL;
+	}
+	else{
+		theClient->Previous->Next = theClient->Next;
+		if(theClient == tail){
+			tail = theClient->Previous;
+		}
+	}
 	free(theClient->public_key);
 	RSA_free(theClient->keypair);
-    free(theClient);
+	free(theClient);
 
-    return NULL;
+	return NULL;
 }
 
 int main(int argc, char **argv)
 {
 	InitRSA();
 	int portNum;
-    int listenfd = 0, connfd = 0;
+	int listenfd = 0, connfd = 0;
 
-    head = tail = NULL;
+	head = tail = NULL;
 
-    if(argc > 2){
-        portNum = atoi(argv[2]);
-    }
-    else{
-        portNum = 5050;
-    }
+	if(argc > 2){
+		portNum = atoi(argv[2]);
+	}
+	else{
+		portNum = 5050;
+	}
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listenfd == -1){
-        printf("Socket retrival failed\n");
-        return -1;
-    }
-    printf("Socket retrival success\n");
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	if(listenfd == -1){
+		printf("Socket retrival failed\n");
+		return -1;
+	}
+	printf("Socket retrival success\n");
 
-    struct sockaddr_in serv_addr;
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(portNum);
+	struct sockaddr_in serv_addr;
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr.sin_port = htons(portNum);
 
-    if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(struct sockaddr)) == -1){
-        printf("Failed to bind port\n");
-        return -1;
-    }
+	if(bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(struct sockaddr)) == -1){
+		printf("Failed to bind port\n");
+		return -1;
+	}
 
-    if(listen(listenfd, 10) == -1){
-        printf("Failed to listen");
-        return -1;
-    }
+	if(listen(listenfd, 10) == -1){
+		printf("Failed to listen");
+		return -1;
+	}
 
-    while(1){
-        connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-        printf("New user has connected\n");
-        Client* newClient = (Client*) malloc(sizeof(Client));
-        newClient->sockfd = connfd;
+	while(1){
+		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
+		printf("New user has connected\n");
+		Client* newClient = (Client*) malloc(sizeof(Client));
+		newClient->sockfd = connfd;
 		newClient->public_key = (char*) malloc(RSA_size(keypair));
 
-        if(head == NULL){
-            head = newClient;
+		if(head == NULL){
+			head = newClient;
 			newClient->Previous = NULL;
-        }
-        else{
-            tail->Next = newClient;
-        }
-        newClient->Previous = tail;
-        tail = newClient;
-        newClient->Next = NULL;
+		}
+		else{
+			tail->Next = newClient;
+		}
+		newClient->Previous = tail;
+		tail = newClient;
+		newClient->Next = NULL;
 
-        pthread_t clientThread;
-        pthread_create(&clientThread, NULL, SomeAwesomeThings, (void*)newClient);
-    }
+		pthread_t clientThread;
+		pthread_create(&clientThread, NULL, SomeAwesomeThings, (void*)newClient);
+	}
 	RSA_free(keypair);
 	free(public_key);
 
-    return 0;
+	return 0;
 }
 
 void InitRSA(){
@@ -287,9 +287,9 @@ void InitRSA(){
 	public_key = pub_key;
 	public_len = pub_len;
 
-	#ifdef PRINT_KEYS
-		printf("%s\n%s\n", pri_key, pub_key);
-	#endif
+#ifdef PRINT_KEYS
+	printf("%s\n%s\n", pri_key, pub_key);
+#endif
 	printf("done.\n");
 	BIO_free_all(pri);
 	BIO_free_all(pub);
